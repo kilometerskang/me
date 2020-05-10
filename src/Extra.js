0ac5713739f5e87f87b99972b167453d198dd3bd
Miles <milesjkang@gmail.com>
2020-05-09 21:01:12 -0700

    More styling adjustments for mobile view.

--- src/Extra.js
+++ src/Extra.js
@@ -5,7 +5,10 @@ function Extra(props) {
     <div className="container">     
       <div className="content">
         <p className="p-txt">
-          I enjoy making music. Find it <a className="link" href="http://smarturl.it/mileskang"><b>here</b></a>.
+          My biggest hobby and side gig is music <span role="img" aria-label="guitar">🎸</span>.
+          Jamming with friends, exploring jazz theory, and composing pieces are pretty much my
+          entire life outside of school.
+          Stream my original music <a className="link" href="http://smarturl.it/mileskang"><b>here</b></a>.
         </p>
       </div> 
     </div>
--- src/Home.js
+++ src/Home.js
@@ -5,15 +5,16 @@ function Projects(props) {
     <div className="container">
       <div className="content">
         <p className="p-txt">
-            I am an undergraduate student at UCLA, studying <b className="txt-accent">Computer Science</b>.
+            I am an undergraduate student at UCLA, pursuing a B.S.
+            in <b className="txt-accent">Computer Science</b>.
         </p>
         <p className="p-txt">
-            My experience in the field comes primarily from coursework, school organizations,
+            My experience in the software field comes primarily from coursework, school organizations,
             and previous/current employment. This website is intended to explain my activities all
             in one place, and hopefully I will keep it up to date.
         </p>
         <p className="p-txt txt-accent">
-            <b>Last updated: 05.03.2020.</b>
+            <b>Last updated: 05.10.2020.</b>
         </p>
       </div>
     </div>
