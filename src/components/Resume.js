a1dc963792bc3fadeaba7bd9bf347759579a3a8d
Miles Kang <milesjkang@gmail.com>
2020-09-11 18:22:33 -0700

    Minor changes.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Pdf from '../documents/resume.pdf'
+import ResumePdf from '../documents/resume.pdf';
  
 function Resume(props) {
   return (
     <div className="container"> 
       <div className="content">
-        <p className="bubble-txt">
-          Find my resume <a className="link" href={Pdf} target="_blank" rel="noopener noreferrer"><b>here</b></a>.
+        <p className="p-txt">
+          Find my resume <a className="link" href={ResumePdf} target="_blank" rel="noopener noreferrer"><b>here</b></a>.
         </p>
       </div>
     </div>
