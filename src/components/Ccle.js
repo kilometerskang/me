48d9ee3e6bb5a3244f227b236dab8ee27fa9e5ef
Miles Kang <milesjkang@gmail.com>
2020-12-04 15:19:23 -0800

    Modify CCLE description

--- src/components/Ccle.js
+++ src/components/Ccle.js
@@ -25,10 +25,13 @@ function Ccle(props) {
         </p>
         <p className="p-txt">
           The application that I worked on is an LTI tool that retrieves the library reserves of a course
-          from the UCLA registrar API and displays it for the user. The frontend interface of the application
-          depends on the role of the user (student or admin), as well as the course information. For example,
-          the 'Analytics' tab would only appear for an admin user, who might use it to track student engagement
-          in the course. I implemented these functionalities using <b>RESTful APIs (Express, Axios)</b> and <b>
+          from the UCLA registrar API, stores it into a local MongoDB database, and displays it (as well as other
+          features) for the user upon loading the application. I wrote update scripts to sync the registrar's data
+          with our own database, and <b>RESTful APIs</b> to easily access the database from the frontend. The
+          interface of the application depends on the role of the user (student or admin), as well as the course
+          department. For example, the 'Analytics' tab would only appear for an admin user, who might use it to track
+          student engagement in the course. Tracking engagement also involved RESTful APIs and storing/pulling student
+          activity in/from our database. I implemented these functionalities using <b>Express.js</b> and <b>
             React hooks
           </b> (whose logic I tested using <b>Jest/Enzyme</b>).
         </p>
@@ -47,7 +50,7 @@ function Ccle(props) {
         <p className="p-txt">
           The development process involved coding, reviewing, and testing (using <b>Jira</b>). We were able to maintain a clean
           and well-documented codebase with <b>ESLint</b> and <b>Github Actions</b>, as well as thorough code reviews and frequent
-          discussion/collaboration.
+          discussion/collaboration with the team.
         </p>
         <span className="p-txt-small">
           <Link className="back-btn" to="/exp">
